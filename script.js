c746ac8851afe286815284da6a5dd7595f799c7c
aitor_mina <aitormina@hotmail.com>
2023-03-01 13:07:16 +0100

    cierran ventanas modales

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const modalButtons = document.querySelectorAll('.show-modal')
+const closeModalButton = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
 
-modalButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector('.modal')
-    const overlay = document.querySelector('.overlay')
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-  })
-})
+// Mostar ventana modal
+function showModal() {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+// Ocultar la ventana modal
+function hideModal() {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+// Añadir listener para los botones que muestran la ventana modal
+modalButtons.forEach((button) => button.addEventListener('click', showModal))
+
+// Añadir listener para el botón que cierra la ventana modal
+closeModalButton.addEventListener('click', hideModal)
